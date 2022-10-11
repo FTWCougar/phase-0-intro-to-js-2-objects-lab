314cafdb5226c0260fb778f015bd5e53388c155f
Cameron <Cameron@dansclan.com>
2022-10-11 14:32:57 -0700

    commit tree 99a785b177fa902c3b920f6bbdf47dc092b0ecdd

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name: "Cameron",
+    streetAddress: "1317 Sprague ST NW"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmployee = {...employee}
+    newEmployee[key] = value;
+    return newEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function  deleteFromEmployeeByKey(employee, key){
+    const newObj = {...employee};
+    delete newObj[key];
+    return newObj;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee;
+}
